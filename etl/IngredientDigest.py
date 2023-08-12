79ba6f340133aa0124214c2a496deeaf7d69981e
Ted Erickson <ted.erickson@comcast.net>
2023-08-12 07:23:24 -0600

    Use Data Classes

--- etl/IngredientDigest.py
+++ etl/IngredientDigest.py
@@ -9,15 +9,6 @@ class IngredientDigest:
     ingredient_serving_unit: str
     ingredient_type: str
 
-    def __str__(self):
-        text = ("IngredientDigest: food_name='{}', description='{}', ingredient_serving_qty={}"
-                ", ingredient_serving_unit={}, ingredient_type='{}'")
-        return text.format(self.food_name,
-                           self.description,
-                           self.ingredient_serving_qty,
-                           self.ingredient_serving_unit,
-                           self.ingredient_type)
-
     def to_json_dictionary(self):
         json = {
             "foodName": self.food_name,
--- etl/InstructionDigest.py
+++ etl/InstructionDigest.py
@@ -6,10 +6,6 @@ class InstructionDigest:
     step_number: int
     instruction: str
 
-    def __str__(self):
-        text = "InstructionDigest: step_number={}, instruction='{}'"
-        return text.format(self.step_number, self.instruction)
-
     def to_json_dictionary(self):
         json = {
             "stepNumber": self.step_number,
